ea71ff0ba6488016df4bb50b03fd35418a685cf8
F.H <54819933+the-code-raccoon@users.noreply.github.com>
2023-07-12 19:48:54 -0400

    Add scss

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -12,45 +12,48 @@ import "./App.css";
 import Landing from "./Landing";
 
 export default function App() {
-  return (
-    <ParallaxProvider scrollAxis="horizontal">
-      <div
-        className="w-[1200vw] sm:w-[885vw] md:w-[810vw] lg:w-[620vw] xl:w-[520vw] bg-gradient-to-b to-white via-[#78A7FF] from-[#78A7FF] dark:bg-gradient-to-b dark:to-black dark:via-[#38285c] dark:from-[#38285c]"
-        onWheel={(e) => {
-          document.scrollingElement.scrollLeft += e.deltaY * 2.5;
-        }}
-      >
-        <Header />
-        <div className="z-10 flex h-[100vh]">
-          <div className="z-10 w-[100vw]" id="landing">
-            <Landing />
-          </div>
+    return (
+        <ParallaxProvider scrollAxis="horizontal">
+            <div
+                className="w-[1200vw] bg-gradient-to-b from-[#78A7FF] via-[#78A7FF] to-white dark:bg-gradient-to-b dark:from-[#38285c] dark:via-[#38285c] dark:to-black sm:w-[885vw] md:w-[810vw] lg:w-[620vw] xl:w-[520vw]"
+                onWheel={(e) => {
+                    document.scrollingElement.scrollLeft += e.deltaY * 2.5;
+                }}
+            >
+                <Header />
+                <div className="z-10 flex h-[100vh]">
+                    <div className="z-10 w-[100vw]" id="landing">
+                        <Landing />
+                    </div>
 
-          <div className="z-10 w-[250vw] sm:w-[200vw] lg:w-[100vw]" id="about">
-            <About />
-          </div>
+                    <div
+                        className="z-10 w-[250vw] sm:w-[200vw] lg:w-[100vw]"
+                        id="about"
+                    >
+                        <About />
+                    </div>
 
-          <div
-            className="z-10 w-[300vw] sm:w-[185vw] md:w-[160vw] lg:w-[120vw] xl:w-[100vw]"
-            id="skills"
-          >
-            <Skills />
-          </div>
+                    <div
+                        className="z-10 w-[300vw] sm:w-[185vw] md:w-[160vw] lg:w-[120vw] xl:w-[100vw]"
+                        id="skills"
+                    >
+                        <Skills />
+                    </div>
 
-          <div
-            className="z-10 w-[450vw] sm:w-[300vw] md:w-[250vw] lg:w-[200vw] xl:w-[120vw]"
-            id="featured-works"
-          >
-            <FeaturedWork />
-          </div>
+                    <div
+                        className="z-10 w-[450vw] sm:w-[300vw] md:w-[250vw] lg:w-[200vw] xl:w-[120vw]"
+                        id="featured-works"
+                    >
+                        <FeaturedWork />
+                    </div>
 
-          <div className="z-10 w-[100vw]" id="contact">
-            <Contact />
-          </div>
+                    <div className="z-10 w-[100vw]" id="contact">
+                        <Contact />
+                    </div>
 
-          <Background />
-        </div>
-      </div>
-    </ParallaxProvider>
-  );
+                    <Background />
+                </div>
+            </div>
+        </ParallaxProvider>
+    );
 }
--- src/App/Contact.tsx
+++ src/App/Contact.tsx
@@ -1,55 +1,63 @@
 export default function Contact() {
-  return (
-    <div className="z-10 flex h-[100vh] w-[100%] justify-center text-slate-900 dark:text-white">
-      <div className="mt-[17vh] flex w-[85%] h-[50vh] flex-col text-center justify-between">
-        <div>
-          <h1>Contact</h1>
+    return (
+        <div className="z-10 flex h-[100vh] w-[100%] justify-center text-slate-900 dark:text-white">
+            <div className="mt-[17vh] flex h-[50vh] w-[85%] flex-col justify-between text-center">
+                <div>
+                    <h1>Contact</h1>
+                </div>
+                <div>
+                    <h1 className="text-base leading-loose sm:text-2xl">
+                        Please feel free to contact me via email at
+                    </h1>
+                </div>
+                <div className="mx-auto">
+                    <a
+                        href="mailto:francesca.ho459@gmail.com"
+                        className="text-black no-underline"
+                    >
+                        <button className="delay-80 flex rounded-md bg-white py-3 px-4 text-center shadow transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-purple-300">
+                            <h1 className="text-sm sm:text-2xl">
+                                francesca.ho459@gmail.com
+                            </h1>
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <h1 className="text-base leading-loose sm:text-2xl">
+                        or on my socials on
+                    </h1>
+                </div>
+                <div className="mx-auto flex flex-col content-center justify-center sm:flex-row">
+                    <div className="mx-auto mb-6 sm:mx-3">
+                        <a
+                            href="https://github.com/the-code-raccoon"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-black no-underline"
+                        >
+                            <button className="delay-80 flex rounded-md bg-slate-500 py-3 px-4 text-center shadow transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-slate-400">
+                                <h1 className="text-base sm:text-2xl">
+                                    Github
+                                </h1>
+                            </button>
+                        </a>
+                    </div>
+                    <div className="sm:mx-3">
+                        <a
+                            href="https://www.linkedin.com/in/francesca-h-111714174/"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-black no-underline transition hover:text-purple-500"
+                        >
+                            <button className="delay-80 flex rounded-md bg-[#0072b1] py-3 px-4 text-center shadow transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-blue-500">
+                                <h1 className="text-base sm:text-2xl">
+                                    Linkedin
+                                </h1>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-          <h1 className="text-base sm:text-2xl leading-loose">
-            Please feel free to contact me via email at
-          </h1>
-        </div>
-        <div className="mx-auto">
-          <a
-            href="mailto:francesca.ho459@gmail.com"
-            className="text-black no-underline"
-          >
-            <button className="delay-80 flex rounded-md bg-white py-3 px-4 text-center shadow transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-purple-300">
-              <h1 className="text-sm sm:text-2xl">francesca.ho459@gmail.com</h1>
-            </button>
-          </a>
-        </div>
-        <div>
-          <h1 className="text-base sm:text-2xl leading-loose">or on my socials on</h1>
-        </div>
-        <div className="flex flex-col sm:flex-row mx-auto justify-center content-center">
-          <div className="mx-auto mb-6 sm:mx-3">
-            <a
-              href="https://github.com/the-code-raccoon"
-              target="_blank"
-              rel="noreferrer"
-              className="text-black no-underline"
-            >
-              <button className="delay-80 flex rounded-md bg-slate-500 py-3 px-4 text-center shadow transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-slate-400">
-                <h1 className="text-base sm:text-2xl">Github</h1>
-              </button>
-            </a>
-          </div>
-          <div className="sm:mx-3">
-            <a
-              href="https://www.linkedin.com/in/francesca-h-111714174/"
-              target="_blank"
-              rel="noreferrer"
-              className="text-black no-underline transition hover:text-purple-500"
-            >
-              <button className="delay-80 flex rounded-md bg-[#0072b1] py-3 px-4 text-center shadow transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-blue-500">
-                <h1 className="text-base sm:text-2xl">Linkedin</h1>
-              </button>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
